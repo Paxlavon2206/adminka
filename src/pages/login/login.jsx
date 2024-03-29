189c0a61afae603af0fad9476831a6bf2c45739e
Paxlavon2206 <paxlavon2206@gmail.com>
2024-03-29 07:20:41 +0500

    dars

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import Input from "../../components/input/input";
+import { useLogin } from './service/useLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Login = () => {
-    const { register, handleSubmit } = useForm();
-    const submit = () => {};
+    const { register, reset, handleSubmit } = useForm();
+    const { mutate, isPending } = useLogin();
+
+    const submit = async (data) => {
+        mutate(data,{
+            onSuccess:(res)=>{
+                console.log(res);
+            }
+        })
+        try {
+            await mutate(data);
+            // If login is successful, show a success toast
+            toast.success('Login successful!');
+            // Reset the form after successful login
+            reset();
+        } catch (error) {
+            // If login fails, show an error toast
+            toast.error('Login failed. Please try again.');
+        }
+    };
 
     return (
         <div className=" bg-admin-gradient fixed inset-0">
@@ -14,17 +35,20 @@ export const Login = () => {
                     type="email" 
                     className='border w-full mb-3 py-1 px-1 rounded-lg'
                     name="email"
+                    defaultValue="paxlavon2206@gmail.com"
                     placeholder="Email"
                 />
                 <Input 
                     {...register('password', { required: true })} 
-                    type="current-password" 
+                    type="password" 
                     className='border w-full py-1 px-1 mb-3 rounded-lg'
                     name="password"
+                    defaultValue="2206"
                     placeholder="Password"
                 />
                 <button className='w-full bg-loginBg text-white py-1 rounded-lg hover:scale-105'>Login</button>
             </form>
+            <ToastContainer /> {/* This component renders the toast notifications */}
         </div>
     );
 };
